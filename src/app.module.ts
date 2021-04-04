c944ac00aae6343683b5faa37572abbdbf85b029
mio <centerfold.15@icloud.com>
2021-04-04 21:53:55 +0900

    Add users GraphQL CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/users/users.resolver.ts
@@ -0,0 +1,44 @@
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
+import { GetUserArgs } from './dto/args/get-user.args';
+import { GetUsersArgs } from './dto/args/get-users.args';
+import { CreateUserInput } from './dto/input/create-user.input';
+import { DeleteUserInput } from './dto/input/delete-user.input';
+import { UpdateUserInput } from './dto/input/update-user.input';
+import { User } from './entity/user';
+import { UsersService } from './users.service';
+
+@Resolver(() => User)
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Query(() => User, { name: 'user', nullable: true })
+  async gerUser(@Args() getUserArgs: GetUserArgs): Promise<User> {
+    return this.usersService.getUser(getUserArgs);
+  }
+
+  @Query(() => [User], { name: 'users', nullable: 'items' })
+  async getUsers(@Args() getUsersArgs: GetUsersArgs): Promise<User[]> {
+    return this.usersService.getUsers(getUsersArgs);
+  }
+
+  @Mutation(() => User)
+  async createUser(
+    @Args('createUserData') createUserData: CreateUserInput,
+  ): Promise<User> {
+    return this.usersService.createUser(createUserData);
+  }
+
+  @Mutation(() => User)
+  async updateUser(
+    @Args('updateUserData') updateUserData: UpdateUserInput,
+  ): Promise<User> {
+    return this.usersService.updateUser(updateUserData);
+  }
+
+  @Mutation(() => User)
+  async deleteUser(
+    @Args('deleteUserDate') deleteUserData: DeleteUserInput,
+  ): Promise<User> {
+    return this.usersService.deleteUser(deleteUserData);
+  }
+}
